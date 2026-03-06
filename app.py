4bb4f3b475229348764ff43ec291993f7a5e04b7
Haris-Mughal <harismirza3456@gmail.com>
2026-03-06 09:26:32 +0500

    added streamlit page config

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="RAG-Explorer-AI-Document-Assistant",
+    page_icon="📄",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
